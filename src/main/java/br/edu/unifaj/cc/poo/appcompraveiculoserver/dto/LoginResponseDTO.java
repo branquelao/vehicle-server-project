d435c450220b1d06a2f3f6a1ba07358c6bf354eb
branquelao <henriquetozzonarciso@gmail.com>
2026-07-16 18:06:12 -0300

    feat: removed pure text LoginRepository query and add LoginResponseDTO

---
+++ src/main/java/br/edu/unifaj/cc/poo/appcompraveiculoserver/dto/LoginResponseDTO.java
@@ -0,0 +1,28 @@
+package br.edu.unifaj.cc.poo.appcompraveiculoserver.dto;
+
+import br.edu.unifaj.cc.poo.appcompraveiculoserver.entities.Login;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseDTO {
+    private Long id;
+    private String usuario;
+    private String telefone;
+    private float carteira;
+    private String loginImagem;
+    private LocalDateTime loginCriadoEm;
+    private LocalDateTime loginAtualizadoEm;
+
+    public static LoginResponseDTO fromEntity(Login login) {
+        return new LoginResponseDTO(
+                login.getId(), login.getUsuario(), login.getTelefone(),
+                login.getCarteira(), login.getLoginImagem(),
+                login.getLoginCriadoEm(), login.getLoginAtualizadoEm()
+        );
+    }
+}
--- src/main/java/br/edu/unifaj/cc/poo/appcompraveiculoserver/repositories/LoginRepository.java
+++ src/main/java/br/edu/unifaj/cc/poo/appcompraveiculoserver/repositories/LoginRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface LoginRepository extends JpaRepository<Login, Long> {
-    Login findByUsuario(String usuario);
-    Optional<Login> findByUsuarioAndSenha(String usuario, String senha);
+    Optional<Login> findByUsuario(String usuario);
 }
