72dab7a9c52eae328be9a78c2ca0f6bfb3f93447
branquelao <henriquetozzonarciso@gmail.com>
2026-07-17 17:39:37 -0300

    feat: add missing resource and missing image exceptions

---
+++ src/main/java/br/edu/unifaj/cc/poo/appcompraveiculoserver/exceptions/ImagemInvalidaException.java
@@ -0,0 +1,7 @@
+package br.edu.unifaj.cc.poo.appcompraveiculoserver.exceptions;
+
+public class ImagemInvalidaException extends RuntimeException {
+    public ImagemInvalidaException(String mensagem) {
+        super(mensagem);
+    }
+}
---
+++ src/main/java/br/edu/unifaj/cc/poo/appcompraveiculoserver/exceptions/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package br.edu.unifaj.cc.poo.appcompraveiculoserver.exceptions;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
