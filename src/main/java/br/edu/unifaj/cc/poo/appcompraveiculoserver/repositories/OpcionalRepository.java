a792ace1ecf0c543af23ab0ac532d5db34400124
branquelao <henriquetozzonarciso@gmail.com>
2026-07-30 19:31:37 -0300

    feat: add VeiculoRepository and OpcionalRepository

---
+++ src/main/java/br/edu/unifaj/cc/poo/appcompraveiculoserver/repositories/OpcionalRepository.java
@@ -0,0 +1,7 @@
+package br.edu.unifaj.cc.poo.appcompraveiculoserver.repositories;
+
+import br.edu.unifaj.cc.poo.appcompraveiculoserver.entities.Opcional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OpcionalRepository extends JpaRepository<Opcional, Long> {
+}
