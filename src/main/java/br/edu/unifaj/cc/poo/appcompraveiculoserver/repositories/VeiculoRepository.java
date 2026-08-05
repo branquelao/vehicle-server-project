ad79c4b364d37b3b2c5ec2132539292e2d0a2cdb
branquelao <henriquetozzonarciso@gmail.com>
2026-08-05 16:40:37 -0300

    feat: extend VeiculoRepository with JpaSpecificationExecutor for pagination support

--- src/main/java/br/edu/unifaj/cc/poo/appcompraveiculoserver/repositories/VeiculoRepository.java
+++ src/main/java/br/edu/unifaj/cc/poo/appcompraveiculoserver/repositories/VeiculoRepository.java
@@ -1,16 +1,14 @@
 package br.edu.unifaj.cc.poo.appcompraveiculoserver.repositories;
 
 import br.edu.unifaj.cc.poo.appcompraveiculoserver.entities.Veiculo;
-import br.edu.unifaj.cc.poo.appcompraveiculoserver.entities.enums.StatusAnuncio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long>, JpaSpecificationExecutor<Veiculo> {
 
     List<Veiculo> findTop3ByOrderByIdDesc();
 
     List<Veiculo> findByLogin_Id(Long loginId);
-
-    List<Veiculo> findByCidadeAndStatus(String cidade, StatusAnuncio status);
 }
